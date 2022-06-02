f87ba9b8764c7855e6e56bf4dc3a4d1e2c87c0a9
Cinthia Fontoura <cinthiafontouras@gmail.com>
2022-06-02 14:03:29 +0000

    Add validation to check for exactly 6 values in the provided data

--- run.py
+++ run.py
@@ -1,5 +1,3 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 import gspread
 from google.oauth2.service_account import Credentials
@@ -10,21 +8,39 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive"
     ]
 
-CREDS = Credentials.from_service_account_file('creds.json')    
+CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
-def get_sales_data():
-  """
-  Get sales figures input from the user
-  """
-  print("Please enter sales data from the last market.")
-  print("Data should be sex numbers, separeted by commas.")
-  print("Example: 10,20,30,40,50,60\n")
 
-  data_str = input("Enter your data here: ")
-  print(f"\n\nThe data provided is {data_str}\n\n")
+def get_sales_data():
+    """
+    Get sales figures input from the user
+    """
+    print("Please enter sales data from the last market.")
+    print("Data should be sex numbers, separeted by commas.")
+    print("Example: 10,20,30,40,50,60\n")
+
+    data_str = input("Enter your data here: ")
+    
+    sales_data = data_str.split(",")
+    validate_data(sales_data)
+
+
+def validate_data(values):
+    """
+    Inside the try, conversts all string into intergers. Paises ValueError if strings cannot be converted into int, or if there aren't exactly 6 values.
+    """
+    try:
+        if len(values) != 6:
+            raise ValueError(
+                f"Exactly 6 values required, you provided {len(values)}"
+            )
+    except ValueError as e:
+        print(f"invalid data: {e}, please try again.\n")    
 
 
 get_sales_data()
+
+
